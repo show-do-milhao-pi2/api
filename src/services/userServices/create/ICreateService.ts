bb68e26779d3e845e11d700f1afad100aa4f53e6
João Matheus <joaomatheusantunes@gmail.com>
2022-10-01 23:09:43 -0300

    resolvendo conflito com a master

--- src/services/userServices/create/ICreateService.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../../../entities/User";
-import { Encrypter } from "../../../utils/encrypter/IEncrypter";
-
-export interface ICreateService {
-  execute(data: User, encrypter: Encrypter): Promise<User>;
-}
